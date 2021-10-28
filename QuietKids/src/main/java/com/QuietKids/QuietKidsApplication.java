218c4a218fd88928891af241446d1a030dba5bf9
Nick Hollins <nholl22@wgu.edu>
2021-10-28 15:52:24 -0500

    initial upload

---
+++ QuietKids/src/main/java/com/QuietKids/QuietKidsApplication.java
@@ -0,0 +1,13 @@
+package com.QuietKids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuietKidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuietKidsApplication.class, args);
+	}
+
+}
---
+++ QuietKids/src/test/java/com/QuietKids/QuietKidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuietKids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuietKidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
